f34db63a113871400cddbdccaef2b5d64c6c5802
sohail shahzad <sohail.shahzad54@gmail.com>
2020-09-26 20:37:54 +0500

    Updated

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name='Sohail Shahzad' age = {39}/></div>,
   document.getElementById('root')
 );
 
